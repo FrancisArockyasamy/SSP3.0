065805cbb7e57ba7f272e54b4a7bc34907bcf8b7
ravishnu60 <ravishnu60@gmail.com>
2024-05-20 17:34:30 +0530

    Added Cors

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from Admission.app import main as admission_main
 from Alumni import main as alumni_main
 from Communication import main as communication_main
 from Examination.app import main as examination_main
-# from Fee import main as fee_main
+from Fee import main as fee_main
 from Feedback import main as feedback_main
 from Front_office import main as front_office_main
 from Homework import main as homework_main
@@ -19,16 +19,24 @@ from Student_portal import main as student_portal_main
 from Timetable import main as timetable_main
 from Transport import main as transport_main
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app= FastAPI(
     title="SSP-3.0 API"
+)
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"]
 )
 
 
 service= [admin_main, academic_main, admission_main, alumni_main, communication_main,
         examination_main, feedback_main, front_office_main, homework_main, id_card_main,
         inventory_main, lesson_plan_main, payroll_main, staff_attendance_main, student_attendance_main,
-        student_portal_main, timetable_main, transport_main
+        student_portal_main, timetable_main, transport_main, fee_main
 ]
-# fee_main,
 for i in service:
     app.include_router(i.app)
